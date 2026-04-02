eca4a6e68b7c513ef85c7d17778406112d079411
AlegZabivnoy <a.ostanin2008@icloud.com>
2026-04-02 11:00:03 +0300

    маленький багфикс

--- lab4/task4.js
+++ lab4/task4.js
@@ -6,11 +6,11 @@ class BiPriorityQueue {
         this.items.push(value, priority)
         console.log(`Added ${value} With priority ${priority}`)
     }
-    _getInddex(type) {
-        if(this.items.length === 0) retrun -1;
+    _getIndex(type) {
+        if(this.items.length === 0) return -1;
         let targetIdx = 0;
         for(let i = 1; i < this.items.length; i++) {
-            if (type === 'highest' && this.items[i].priority  > this.items[targetIdx].priority {
+            if (type === 'highest' && this.items[i].priority  > this.items[targetIdx].priority) {
                 targetIdx = i;
             } else if (type === 'lowest' && this.items[i].priority < this.items[targetIdx].priority) {
                 targetIdx = i;
