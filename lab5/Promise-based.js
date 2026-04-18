bc9271589d2fce209c9fe8b544da5d2c592c1678
AlegZabivnoy <a.ostanin2008@icloud.com>
2026-04-18 10:47:05 +0300

    Написана Promise-based alternative функция

---
+++ lab5/Promise-based.js
@@ -0,0 +1,19 @@
+function macPromise(array, iteratee, {signal} = {}) {
+    return new Promise((resolve, reject) => {
+
+        if (signal?.aborted) {
+            return reject (new Error("Aborted"))
+        }
+
+        const promises = array.map(async (item) => {
+            if (signal?.aborted) throw new Error("Aborted");
+            return await iteratee(item);
+        });
+
+        signal?.addEventListener("abort", () => {
+            reject(new Error("Aborted"));
+        })
+
+        Promise.all(promises).then(resolve).catch(reject);
+    })
+}
